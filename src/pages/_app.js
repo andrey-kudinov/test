46dbaeed222a7730b4e7910710eab7cf4763aa96
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-12-20 09:13:48 +0400

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "@/styles/reset.scss";
 import "@/styles/global.scss";
-import Home from ".";
 
 export default function App({ Component, pageProps }) {
   return (
